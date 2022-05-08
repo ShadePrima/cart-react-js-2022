64c7bc1185ff3e4260e9961944030590b5c33fa3
ShadePrima <aleksandrag1984@gmail.com>
2022-05-08 09:30:57 +0300

    tranafer style to the component app

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import "./_base.scss"
+import "./_reset.scss"
+import "./_vars.scss"
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, React app</h1>
+    </div>
+  );
+}
+
+export default App;
